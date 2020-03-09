f0aaf89d126f14c981e4982e71b78e086ecf8b45
bumbot <pkim1295@gmail.com>
2020-03-09 10:26:49 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const newTime = parseInt(time.split(":"))
+
+  if (newTime < 12) {
+    return "Good Morning"
+  } else if (newTime >= 12 && newTime <= 17) {
+    return "Good Afternoon"
+  } else if (newTime > 17) {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(msg) {
+  let node = document.getElementById('greeting')
+  
+  node.innerText = msg;
+}
